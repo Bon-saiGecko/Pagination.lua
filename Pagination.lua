cc5420e6021ed574095bd306bb859134754cb2ff
Saige <13684891+Bon-saiGecko@users.noreply.github.com>
2021-11-09 18:09:37 -0500

    Update Pagination.lua

--- Pagination.lua
+++ Pagination.lua
@@ -3,6 +3,21 @@ local Pagination = {}
 function Pagination.Paginate(tbl, pageSize)
 	local newTbl = {}
 	
+	local currentIndex = 1
+	while currentIndex <= #tbl do
+		local newPage = {}
+		for _=1, pageSize do
+			if currentIndex <= #tbl then
+				table.insert(newPage, tbl[currentIndex])
+				currentIndex = currentIndex + 1
+			end
+		end
+		table.insert(newTbl, {
+			pageNumber = #newTbl + 1,
+			page = newPage
+		})
+	end
+
 	return newTbl
 end
 
